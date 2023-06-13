75eb678da3ad667e60d09735235fec961dcc3a6d
GabiEngineer <gabrielcoutinho.pro@gmail.com>
2023-06-13 17:13:11 -0300

    feat: adicionando resultado do cálculo

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+const peso = document.getElementById("peso");
+const altura = document.getElementById("altura");
+const calcular = document.getElementById("calcular");
+
+calcular.addEventListener("click", function () {});
